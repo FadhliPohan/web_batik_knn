5cef083550f042840eeecf2a7443996c979ec6bc
i-qof <muhammadfadly.mfd@gmail.com>
2025-01-15 14:57:32 +0700

    penyesuaian templet

--- templete/head.php
+++ templete/head.php
@@ -71,3 +71,203 @@ $ks12 = "Des";
 
 ?>
 
+
+<?php function RP($rupiah)
+{
+  return number_format($rupiah, "2", ",", ".");
+} ?>
+<?php
+function WKT($sekarang)
+{
+  $tanggal = substr($sekarang, 8, 2) + 0;
+  $bulan = substr($sekarang, 5, 2);
+  $tahun = substr($sekarang, 0, 4);
+
+  $judul_bln = array(1 => "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+  $wk = $tanggal . " " . $judul_bln[(int) $bulan] . " " . $tahun;
+  return $wk;
+}
+?>
+<?php
+function WKTP($sekarang)
+{
+  $tanggal = substr($sekarang, 8, 2) + 0;
+  $bulan = substr($sekarang, 5, 2);
+  $tahun = substr($sekarang, 2, 2);
+
+  $judul_bln = array(1 => "Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des");
+  $wk = $tanggal . " " . $judul_bln[(int) $bulan] . "'" . $tahun;
+  return $wk;
+}
+?>
+<?php
+function BAL($tanggal)
+{
+  $arr = explode(" ", $tanggal);
+  if ($arr[1] == "Januari" || $arr[1] == "January") {
+    $bul = "01";
+  } else if ($arr[1] == "Februari" || $arr[1] == "February") {
+    $bul = "02";
+  } else if ($arr[1] == "Maret" || $arr[1] == "March") {
+    $bul = "03";
+  } else if ($arr[1] == "April") {
+    $bul = "04";
+  } else if ($arr[1] == "Mei" || $arr[1] == "May") {
+    $bul = "05";
+  } else if ($arr[1] == "Juni" || $arr[1] == "June") {
+    $bul = "06";
+  } else if ($arr[1] == "Juli" || $arr[1] == "July") {
+    $bul = "07";
+  } else if ($arr[1] == "Agustus" || $arr[1] == "August") {
+    $bul = "08";
+  } else if ($arr[1] == "September") {
+    $bul = "09";
+  } else if ($arr[1] == "Oktober" || $arr[1] == "October") {
+    $bul = "10";
+  } else if ($arr[1] == "November") {
+    $bul = "11";
+  } else if ($arr[1] == "Nopember") {
+    $bul = "11";
+  } else if ($arr[1] == "Desember" || $arr[1] == "December") {
+    $bul = "12";
+  }
+  return "$arr[2]-$bul-$arr[0]";
+}
+?>
+
+
+<?php
+function BALP($tanggal)
+{
+  $arr = split(" ", $tanggal);
+  if ($arr[1] == "Jan") {
+    $bul = "01";
+  } else if ($arr[1] == "Feb") {
+    $bul = "02";
+  } else if ($arr[1] == "Mar") {
+    $bul = "03";
+  } else if ($arr[1] == "Apr") {
+    $bul = "04";
+  } else if ($arr[1] == "Mei") {
+    $bul = "05";
+  } else if ($arr[1] == "Jun") {
+    $bul = "06";
+  } else if ($arr[1] == "Jul") {
+    $bul = "07";
+  } else if ($arr[1] == "Agu") {
+    $bul = "08";
+  } else if ($arr[1] == "Sep") {
+    $bul = "09";
+  } else if ($arr[1] == "Okt") {
+    $bul = "10";
+  } else if ($arr[1] == "Nov") {
+    $bul = "11";
+  } else if ($arr[1] == "Nop") {
+    $bul = "11";
+  } else if ($arr[1] == "Des") {
+    $bul = "12";
+  }
+  return "$arr[2]-$bul-$arr[0]";
+}
+?>
+
+
+<?php
+function process($conn, $sql)
+{
+  $s = false;
+  $conn->autocommit(FALSE);
+  try {
+    $rs = $conn->query($sql);
+    if ($rs) {
+      $conn->commit();
+      $last_inserted_id = $conn->insert_id;
+      $affected_rows = $conn->affected_rows;
+      $s = true;
+    }
+  } catch (Exception $e) {
+    echo 'fail: ' . $e->getMessage();
+    $conn->rollback();
+  }
+  $conn->autocommit(TRUE);
+  return $s;
+}
+
+function getJum($conn, $sql)
+{
+
+  $rs = $conn->query($sql);
+  $jum = $rs->num_rows;
+  $rs->free();
+  return $jum;
+}
+
+function getField($conn, $sql)
+{
+  $rs = $conn->query($sql);
+  $rs->data_seek(0);
+  $d = $rs->fetch_assoc();
+  $rs->free();
+  return $d;
+}
+
+function getData($conn, $sql)
+{
+  $rs = $conn->query($sql);
+  $rs->data_seek(0);
+  $arr = $rs->fetch_all(MYSQLI_ASSOC);
+  //foreach($arr as $row) {
+  //  echo $row['nama_kelas'] . '*<br>';
+  //}
+
+  $rs->free();
+  return $arr;
+}
+
+
+function getUser($conn, $kode)
+{
+  $field = "nama_user";
+  $sql = "SELECT `$field` FROM `tb_user` where `id_user`='$kode'";
+  $rs = $conn->query($sql);
+  $rs->data_seek(0);
+  $row = $rs->fetch_assoc();
+  $rs->free();
+  return $row[$field];
+}
+
+
+function getToko($conn, $kode)
+{
+  $field = "nama_toko";
+  $sql = "SELECT `$field` FROM `tb_toko` where `id_toko`='$kode'";
+  $rs = $conn->query($sql);
+  $rs->data_seek(0);
+  $row = $rs->fetch_assoc();
+  $rs->free();
+  return $row[$field];
+}
+
+function getIToko($conn, $kode)
+{
+  $kode = trim($kode);
+  $kode = str_replace("Data dari Toko ", "", $kode);
+  $kode = trim($kode);
+
+  $field = "id_toko";
+  $sql = "SELECT `$field` FROM `tb_toko` where `nama_toko` like '%$kode%'";
+  $ada = getJum($conn, $sql);
+  if ($ada < 1) {
+    $ar = explode(" ", $kode);
+    $jml = count($ar) - 1;
+    $kode = $ar[$jml];
+    $sql = "SELECT `$field` FROM `tb_toko` where `nama_toko` like '%$kode%'";
+  }
+  $rs = $conn->query($sql);
+  $rs->data_seek(0);
+  $row = $rs->fetch_assoc();
+  $rs->free();
+  return $row[$field];
+}
+?>
+
